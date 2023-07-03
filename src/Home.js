aa3f1879ddead312001829ffc8b14e0d9b8357a1
Nisanth Kumar <nisanth.kumar@itraveller.com>
2023-07-03 10:00:47 +0530

    Update .gitignore and unstage files

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,25 @@ import React from 'react';
 import LotusImage from "./assets/images/natamai_a_hyperreal_lotus_jewelery_with_diamonds_and_pearl_3d_c_461f284f-ff5f-4449-bb4c-815367914787.png";
 const Home = () => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <img src={LotusImage} alt='Lotus' />
+        <div className="limiter">
+            <div className="container-loginhome">
+                <div className="wrap-loginhome">
+                    <div className='form-index'>
+                        <div class="container-loginhome-form-btn mb-3">
+                            <a href='/login' class="loginhome-form-btn">
+                                Login
+                            </a>
+                        </div>
+                        <div class="container-loginhome-form-btn">
+                            <a href='/register' class="loginhome-form-btn">
+                                Register
+                            </a>
+                        </div>
+                    </div>
+                    <div className='loginhome-more' style={{ backgroundImage: `url(${LotusImage})` }}>
+
+                    </div>
+
                 </div>
             </div>
         </div>
